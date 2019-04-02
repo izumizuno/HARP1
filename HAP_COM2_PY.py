11f41b8aa0f7ee151db1c6f975d8ffe08923f8f3
Izumi Mizuno <i.mizuno@eaobservatory.org>
2019-04-01 16:37:26 -1000

    add twobin_ht_func2
    input dataX and dataY so having more flexiblity than twobin_ht_func2

--- HAP_COM2_PY.py
+++ HAP_COM2_PY.py
@@ -60,3 +60,57 @@ def twobin_ht_func(data, binsep, nbin_freq, out_dir, sideband):
     plt.savefig(out_dir+out_name_trx+sideband+'.png')
     return(ht_lof, ht_lo_trx)
 
+
+#--------function
+def twobin_ht_func2(dataX, dataY,  binsep, nbin_X, out_dir, sideband):
+    out_name_X='sep'+str(nbin_X)
+    out_name_Y='bin'+str(np.diff(binsep)[1])
+    #nbin_X=70 # bin number for hist of lo frequency
+    #binsep=np.arange(0,1050,50) #set the bin for trx histogram
+
+    fig=plt.figure()
+    ax = fig.add_subplot(211)
+    ht_=ax.hist(dataX,bins=nbin_X, color='red')
+    ax2 = fig.add_subplot(212)
+    ax2.scatter(dataX,dataY, color='red', s=1)
+    plt.savefig(out_dir+out_name_X+sideband+'.png')
+
+    #set bin for frequency
+    count_lof=ht_[0]
+    separation=ht_[1]
+    sep_diff=np.diff(separation)
+    center=separation[:-1]+sep_diff/2
+
+    ht_X=pd.DataFrame({'h_X':center,
+             'count':ht_[0]})
+    
+    nbin_Y=len(binsep)-1 
+    histX=pd.DataFrame({'h_X':np.zeros(nbin_X*nbin_Y),
+             'h_Y':np.zeros(nbin_X*nbin_Y),
+            'count':np.zeros(nbin_X*nbin_Y),
+                   'percent':np.zeros(nbin_X*nbin_Y)})
+    
+    for i in np.arange(nbin_X):
+        if  count_lof[i]!=0:
+            h_data=dataY[(dataX>=separation[i]) & (dataX<separation[i+1])]
+            fig=plt.figure()
+            ax=fig.add_subplot(221)
+            h_result=ax.hist(h_data,bins=binsep)
+
+            ax2 = fig.add_subplot(212)
+            ax2.scatter(dataX,dataY, color='red', s=1)
+
+            start=i*nbin_Y
+            end=i*nbin_Y+(nbin_Y-1)
+            histX.loc[start:end,'count']=h_result[0]
+            histX.loc[start:end,'percent']=h_result[0]/count_lof[i]
+            histX.loc[start:end,'h_Y']=h_result[1][:-1]+np.diff(h_result[1])/2
+            histX.loc[start:end,'h_X']=center[i]
+    ht_X_y=histX[histX['count']!=0]
+    fig=plt.figure()
+    ax=fig.add_subplot(111)
+    sc=ax.scatter(x=ht_X_y['h_X'], y=ht_X_y['h_Y'], c=ht_X_y['percent'],s=8)
+    plt.colorbar(sc)
+    plt.savefig(out_dir+out_name_Y+sideband+'.png')
+    return(ht_X, ht_X_y)
+
